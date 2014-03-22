fe43c6280b30f2a55475b67150bf95495b6ae8ea
zippera <sovey198@gmail.com>
2014-03-22 10:50:24 +0800

    comment hsv

--- hsv_hist.m
+++ hsv_hist.m
@@ -2,4 +2,4 @@ function h = hsv_hist(im)
 
   im = cv.cvtColor(im,'RGB2HSV');
   edges = {linspace(0,180,30+1),linspace(0,256,32+1)};
-  h = cv.calcHist(im(:,:,1:2), edges);
+  h = cv.calcHist(im(:,:,1:2), edges);%只取 h 和 s，edges 是其分别的 bin 标准
